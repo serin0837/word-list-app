78fb05a19d75c3df4bf25817c4f70be94085e0db
Serin <serin0837@yahoo.com>
2020-12-01 20:40:36 +0000

    refactor test

--- src/component/__tests__/WordCard.test.js
+++ src/component/__tests__/WordCard.test.js
@@ -2,51 +2,31 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import WordCard from '../WordCard'
 
-
-test('The word card shows the word name', () => {
-    // Arrange
+describe("rendered word card would work with different properties",() =>{
     const sampleWord = {
         name : "Cha",
         meaning: "Tea",
         note: "Some Note"
     }
-    // Action
-    const screen = render(<WordCard word={sampleWord}/>)
-
-    // Assertion
-    const wordNameElement = screen.getByText(sampleWord.name, { exact: false })
-
-    expect(wordNameElement).toBeDefined()
-})
-
-test('The word card shows the word meaning', () => {
-    // Arrange
-    const sampleWord = {
-        name : "Cha",
-        meaning: "Tea",
-        note: "Some Note"
-    }
-    // Action
-    const screen = render(<WordCard word={sampleWord}/>)
-
-    // Assertion
-    const wordMeaningElement = screen.getByText(sampleWord.meaning, { exact: false })
-
-    expect(wordMeaningElement).toBeDefined()
-})
-
-test('The word card shows the word note', () => {
-    // Arrange
-    const sampleWord = {
-        name : "Cha",
-        meaning: "Tea",
-        note: "Some Note"
-    }
-    // Action
-    const screen = render(<WordCard word={sampleWord}/>)
-
-    // Assertion
-    const wordNoteElement = screen.getByText(sampleWord.note, { exact: false })
-
-    expect(wordNoteElement).toBeDefined()
+    let screen 
+    //run before every single unit test//beforeEach
+    beforeEach(()=>{
+        screen = render(<WordCard word={sampleWord}/>)
+    })
+    test('The word card shows the word name', () => {
+        // Assertion
+        const wordNameElement = screen.getByText(sampleWord.name, { exact: false })
+        expect(wordNameElement).toBeDefined()
+    })
+    
+    test('The word card shows the word meaning', () => {
+        const wordMeaningElement = screen.getByText(sampleWord.meaning, { exact: false })
+        expect(wordMeaningElement).toBeDefined()
+    })
+    
+    test('The word card shows the word note', () => {
+        // Assertion
+        const wordNoteElement = screen.getByText(sampleWord.note, { exact: false })
+        expect(wordNoteElement).toBeDefined()
+    })
 })
